0be05e2fd725f11c4dcfbd81acf1b19c635601eb
劉明凱 Matt Liu <matt.liu@tpisoftware.com>
2021-05-17 13:04:20 +0800

    Matt Test deeplink

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -4,6 +4,14 @@
     <router-link :to="{name: 'login'}">
       <p>Login</p>
     </router-link>
+
+    <ul>
+      <li><a href="scmbtw://">scmbtw://</a></li>
+      <li><a href="scmbtw://*">scmbtw://*</a></li>
+      <li><a href="scmbtw://fund/fundMyWatch?fundId=123456">scmbtw://fund/fundMyWatch?fundId=123456</a></li>
+      <li><a href="scmbtw://login">scmbtw://login</a></li>
+    </ul>
+
   </div>
 </template>
 
