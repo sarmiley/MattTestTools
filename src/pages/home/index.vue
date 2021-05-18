77966e7ddc1287434fc362fcbd21c0663b6a628f
劉明凱 Matt Liu <matt.liu@tpisoftware.com>
2021-05-18 10:35:13 +0800

    test deep link

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,17 +1,64 @@
 <template>
   <div id="home">
-    <h1>登入</h1>
-    <router-link :to="{name: 'login'}">
-      <p>Login</p>
-    </router-link>
-
-    <ul>
-      <li><a href="scmbtw://">scmbtw://</a></li>
-      <li><a href="scmbtw://*">scmbtw://*</a></li>
-      <li><a href="scmbtw://fund/fundMyWatch?fundId=123456">scmbtw://fund/fundMyWatch?fundId=123456</a></li>
-      <li><a href="scmbtw://login">scmbtw://login</a></li>
-    </ul>
-
+    <div>
+      <input v-model="deepUrl" type="text">
+      <button @click="goToUrl()">前往</button>
+    </div>
+    <div>
+      <p>
+        <a href="https://deeplinks-6ea47.web.app/fund/application?fundId=111111"
+          >Deeplinks Demo for fund/application</a
+        >
+      </p>
+      <p>
+        <a href="https://deeplinks-6ea47.web.app/fund/myCompare?fundId=222222"
+          >Deeplinks Demo for fund/myCompare</a
+        >
+      </p>
+      <p>
+        <a href="https://deeplinks-6ea47.web.app/fund/myWatch?fundId=333333"
+          >Deeplinks Demo for fund/myWatch</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://deeplinks-6ea47.web.app/fund/purchaseMultiFundCIP?fundList=999999&fundList=888888&fundList=777777"
+          >Deeplinks Demo for fund/purchaseMultiFundCIP</a
+        >
+      </p>
+    </div>
+    <div>
+      <h1>_blank</h1>
+      <p>
+        <a
+          href="https://deeplinks-6ea47.web.app/fund/application?fundId=111111"
+          target="_blank"
+          >Deeplinks Demo for fund/application</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://deeplinks-6ea47.web.app/fund/myCompare?fundId=222222"
+          target="_blank"
+          >Deeplinks Demo for fund/myCompare</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://deeplinks-6ea47.web.app/fund/myWatch?fundId=333333"
+          target="_blank"
+          >Deeplinks Demo for fund/myWatch</a
+        >
+      </p>
+      <p>
+        <a
+          href="https://deeplinks-6ea47.web.app/fund/purchaseMultiFundCIP?fundList=999999&fundList=888888&fundList=777777"
+          target="_blank"
+        >
+          Deeplinks Demo for fund/purchaseMultiFundCIP
+        </a>
+      </p>
+    </div>
   </div>
 </template>
 
@@ -23,6 +70,16 @@ export default {
   components: {
     HelloWorld,
     MenuBtn
+  },
+  data () {
+    return {
+      deepUrl: ''
+    }
+  },
+  methods: {
+    goToUrl: function () {
+      window.open(this.deepUrl, '_system')
+    }
   }
 }
 </script>
